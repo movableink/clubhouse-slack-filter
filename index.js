66a2d0c0e09593eddf0c5b3ad1b443ffe66a2183
Michael Nutt <michael@nuttnet.net>
2015-09-03 19:27:56 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,47 @@
+var http = require('http');
+var request = require('request');
+
+var server = http.createServer(function(req, res) {
+  var body = '';
+  req.on('data', function(chunk) {
+    body += chunk;
+  });
+
+  function isAgileDeploy(msg) {
+    return (msg.indexOf('[agileEMAIL]') == 0) && msg.match(/Deploy/);
+  }
+
+  req.on('end', function() {
+    if(body[0] != '{') {
+      console.log("NOT JSON! " + body);
+      res.writeHead(500, {});
+      res.end("NO");
+      return;
+    }
+
+    var payload = JSON.parse(body);
+
+    if(isAgileDeploy(payload.text)) {
+      console.log("Posting to clubhouse: " + payload.text);
+      request.post(process.env.SLACK_HOOK_URL, {
+        form: {payload: body}
+      }, function(err, httpResponse, body) {
+        if(err) {
+          console.log(err);
+        } else {
+          console.log("Response from slack: " + httpResponse.body);
+        }
+      });
+    } else {
+      console.log("Not posting to clubhouse: " + payload.text);
+    }
+
+    res.writeHead(200, {});
+    res.end();
+  });
+});
+
+var port = process.env.PORT || 5000;
+server.listen(port, function() {
+  console.log("Listening on " + port);
+});
